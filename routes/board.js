54989aa4b22630b57016581b810e9bbed7bc535e
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-16 11:32:52 +0900

    route, html 파일들 링크 수정

--- routes/board.js
+++ routes/board.js
@@ -2,6 +2,24 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 
+router.get('/list', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'list.html'));
+});
 
+router.get('/write', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'write.html'));
+});
+
+router.get('/view', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'view.html'));
+});
+
+router.get('/delete', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'delete.html'));
+});
+
+router.get('/update', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'update.html'));
+});
 
 module.exports = router;
--- routes/member.js
+++ routes/member.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 
+router.get('/join', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'join.html'));
+});
 
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'login.html'));
+});
+
+router.get('/myinfo', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'myinfo.html'));
+});
 
 module.exports = router;
