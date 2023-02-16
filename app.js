bd8b5dfa122c48ecb7a5582ff7e2f331cab6ec3a
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-16 10:59:54 +0900

    router 재수정 - member, board

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const logger = require('morgan');
 
 // 라우팅 모듈 설정
 const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
-const aboutRouter = require('./routes/about');
+const memberRouter = require('./routes/member');
+const boardRouter = require('./routes/board');
 
 // express 객체 생성 및 포트 변수 선언
 const app = express();
@@ -17,8 +17,8 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 // 라우팅 모듈 등록 - 클라이언트 요청 처리 핵심 파트
 app.use('/', indexRouter);
-app.use('/user', userRouter);
-app.use('/about', aboutRouter);
+app.use('/member', memberRouter);
+app.use('/board', boardRouter);
 
 // 404, 500 응답코드에 대한 라우팅 처리 정의
 app.use((req, res) => {
