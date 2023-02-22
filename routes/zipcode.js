45c1a5b9fefd230446fd931c2943ccc8b5ddad15
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-22 12:46:51 +0900

    시도, 구군, 읍면동 출력1

--- routes/zipcode.js
+++ routes/zipcode.js
@@ -5,15 +5,16 @@ const Zipcode = require('../models/Zipcode');
 router.get('/', async (req, res) => {
 
     let sidos = new Zipcode().getSido().then(sido => sido);
-    console.log(await sidos);
+    //console.log(await sidos);
 
     let guguns = new Zipcode().getGugun('서울').then(gugun => gugun);
-    console.log(await guguns);
+    //console.log(await guguns);
 
     let dongs = new Zipcode().getDong('서울','강남구').then(dong => dong);
-    console.log(await dongs);
+    //console.log(await dongs);
 
-    res.render('zipcode', {title: '시군구동 찾기'});
+    res.render('zipcode', {title: '시군구동 찾기',
+        sidos: await sidos, guguns: await guguns, dongs: await dongs});
 });
 
 module.exports = router;
