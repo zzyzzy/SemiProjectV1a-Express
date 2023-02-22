095cb6d354c1c78ce783a49de51db78dce72ec9a
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-22 10:10:06 +0900

    우편번호 검색앱 - 뷰, 라우팅 생성 및 등록

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const oracledb = require('./models/Oracle');
 const indexRouter = require('./routes/index');
 const memberRouter = require('./routes/member');
 const boardRouter = require('./routes/board');
+const zipcodeRouter = require('./routes/zipcode');
 
 
 // express 객체 생성 및 포트 변수 선언
@@ -63,6 +64,7 @@ app.use(function(req, res, next){
 app.use('/', indexRouter);
 app.use('/member', memberRouter);
 app.use('/board', boardRouter);
+app.use('/zipcode', zipcodeRouter);
 
 
 // 404, 500 응답코드에 대한 라우팅 처리 정의
---
+++ routes/zipcode.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Zipcode = require('../models/Zipcode');
+
+
+
+module.exports = router;
