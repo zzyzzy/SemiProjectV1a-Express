5213df2092d20de9f84c9ea5747659ed2a4f1ef9
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-22 12:33:06 +0900

    zipcode 모델 - 시도, 구군, 읍면동 검색

---
+++ models/Zipcode.js
@@ -0,0 +1,109 @@
+const oracledb = require('./Oracle');
+
+let zipcodesql = {
+    sidosql: ' select distinct sido from zipcode2013 order by sido ',
+    gugunsql: ' select distinct gugun from zipcode2013 ' +
+              ' where sido = :1 order by gugun ',
+    dongsql: ' select distinct dong from zipcode2013 ' +
+             ' where sido = :1 and gugun = :2 order by dong ',
+    zipsql: ' select * from zipcode2013 ' +
+        ' where sido = :1 and gugun = :2 and dong = :3 order by zipcode ',
+}
+
+class Zipcode {
+    constructor() {
+    }
+
+    async getSido() {
+        let conn = null;
+        let params = [];
+        let sidos = [];
+
+        try {
+            conn = await oracledb.makeConn();
+            let result = await conn.execute(zipcodesql.sidosql, params, oracledb.options);
+            let rs = result.resultSet;
+
+            let row = null;
+            while((row = await rs.getRow())) {
+                let sido = {'sido': row.SIDO};
+                sidos.push(sido);
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await oracledb.closeConn(conn);
+        }
+
+        return sidos;
+    }; // getSido
+
+    async getGugun(sido) {
+        let conn = null;
+        let params = [sido];
+        let guguns = [];
+
+        try {
+            conn = await oracledb.makeConn();
+            let result = await conn.execute(zipcodesql.gugunsql, params, oracledb.options);
+            let rs = result.resultSet;
+
+            let row = null;
+            while((row = await rs.getRow())) {
+                let gugun = {'gugun': row.GUGUN};
+                guguns.push(gugun);
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await oracledb.closeConn(conn);
+        }
+
+        return guguns;
+    };
+
+    async getDong(sido, gugun) {
+        let conn = null;
+        let params = [sido, gugun];
+        let dongs = [];
+
+        try {
+            conn = await oracledb.makeConn();
+            let result = await conn.execute(zipcodesql.dongsql, params, oracledb.options);
+            let rs = result.resultSet;
+
+            let row = null;
+            while((row = await rs.getRow())) {
+                let dong = {'dong': row.DONG};
+                dongs.push(dong);
+            }
+
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await oracledb.closeConn(conn);
+        }
+
+        return dongs;
+    };
+
+    async getZipcode() {
+        let conn = null;
+        let params = [];
+        let sidos = [];
+
+        try {
+            conn = await oracledb.makeConn();
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await oracledb.closeConn(conn);
+        }
+
+        return sidos;
+    };
+
+
+};
+
+module.exports = Zipcode;
--- routes/zipcode.js
+++ routes/zipcode.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Zipcode = require('../models/Zipcode');
 
+router.get('/', async (req, res) => {
 
+    let sidos = new Zipcode().getSido().then(sido => sido);
+    console.log(await sidos);
+
+    let guguns = new Zipcode().getGugun('서울').then(gugun => gugun);
+    console.log(await guguns);
+
+    let dongs = new Zipcode().getDong('서울','강남구').then(dong => dong);
+    console.log(await dongs);
+
+    res.render('zipcode', {title: '시군구동 찾기'});
+});
 
 module.exports = router;
