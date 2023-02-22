540ef12c8b9e5083509dec7b5c75ea54bcb973a3
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-22 15:16:57 +0900

    시도, 구군, 읍면동 출력4 - 시도 : 시군구 : 동 : 우편번호 - SSR

--- models/Zipcode.js
+++ models/Zipcode.js
@@ -87,20 +87,31 @@ class Zipcode {
         return dongs;
     };
 
-    async getZipcode() {
+    async getZipcode(sido, gugun, dong) {
         let conn = null;
-        let params = [];
-        let sidos = [];
+        let params = [sido, gugun, dong];
+        let zips = [];
 
         try {
             conn = await oracledb.makeConn();
+            let result = await conn.execute(zipcodesql.zipsql, params, oracledb.options);
+            let rs = result.resultSet;
+
+            let row = null;
+            while((row = await rs.getRow())) {
+                let zip = { 'zipcode': row.ZIPCODE, 'sido':row.SIDO,
+                    'gugun':row.GUGUN, 'dong': row.DONG, 'ri': row.RI,
+                    'bunji': row.BUNJI };
+                zips.push(zip);
+            }
+
         } catch (e) {
             console.log(e);
         } finally {
             await oracledb.closeConn(conn);
         }
 
-        return sidos;
+        return zips;
     };
 
 
--- routes/zipcode.js
+++ routes/zipcode.js
@@ -5,22 +5,26 @@ const Zipcode = require('../models/Zipcode');
 router.get('/', async (req, res) => {
     let sido = req.query.sido;
     let gugun = req.query.gugun;
+    let dong = req.query.dong;
     let [guguns, dongs, zips] = [null, null, null];
 
     let sidos = new Zipcode().getSido().then(sido => sido);
     //console.log(await sidos);
 
-    if (sido !== undefined)
+    if (sido !== undefined)  // 시구군 검색
         guguns = new Zipcode().getGugun(sido).then(gugun => gugun);
     //console.log(await guguns);
 
-    if (sido !== undefined && gugun !== undefined)
+    if (sido !== undefined && gugun !== undefined) // 읍면동 검색
         dongs = new Zipcode().getDong(sido, gugun).then(dong => dong);
     //console.log(await dongs);
 
+    if (sido !== undefined && gugun !== undefined && dong !== undefined)  // 우편번호 검색
+        zips = new Zipcode().getZipcode(sido, gugun, dong).then(zip => zip);
+
     res.render('zipcode', {title: '시군구동 찾기',
         sidos: await sidos, guguns: await guguns, dongs: await dongs,
-        sido: sido, gugun: gugun });
+        sido: sido, gugun: gugun, dong: dong, zips: await zips });
 });
 
 module.exports = router;
