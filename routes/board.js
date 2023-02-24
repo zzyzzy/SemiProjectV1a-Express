1db1f4fbd99ba5d6346c4780c62c75dfe65c8024
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-24 10:48:52 +0900

    게시판 페이징 구현2 - 이전, 다음 버튼 표시

--- routes/board.js
+++ routes/board.js
@@ -14,29 +14,45 @@ const ppg = 15;
 // ednum : stnum + ppg
 router.get('/list', async (req, res) => {
     let { cpg } = req.query;
-    cpg = cpg ? cpg : 1;
+    cpg = cpg ? parseInt(cpg) : 1;
     let stnum = (cpg - 1) * ppg + 1;  // 지정한 페이지 범위 시작값 계산
-    let stpgn = parseInt((cpg - 1) / 10) * 10 + 1; // 페이지네이션 시작값 계산
+
+    let allcnt = new Board().selectCount().then((cnt) => cnt);  // 총게시물 수
+    let alpg = Math.ceil(await allcnt / ppg);  // 총 페이지수 계산
 
     // 페이지네이션 블럭 생성
+    // 1 페이지의 페이지네이션 : 1 2 3 4 5 6 7 8 9 10
+    // 2 페이지의 페이지네이션 : 1 2 3 4 5 6 7 8 9 10
+    // ...
+    // 10 페이지의 페이지네이션 : 1 2 3 4 5 6 7 8 9 10
+    // 11 페이지의 페이지네이션 : 11 12 13 14 15 16 17 18 19 20
+    // ...
+    // 15 페이지의 페이지네이션 : 11 12 13 14 15 16 17 18 19 20
+    // ...
+    // 21 페이지의 페이지네이션 : 21 22 23 24 25 26 27 28 29 30
+    let stpgn = parseInt((cpg - 1) / 10) * 10 + 1; // 페이지네이션 시작값 계산
     let stpgns = [];
-    for (let i = stpgn; i < stpgn + 10; ++i){
-        let iscpg = (i == cpg) ? true : false;  // 현재페이지 표시
-        let pgn = { 'num': i, 'iscpg': iscpg };
-        stpgns.push(pgn);
+    for (let i = stpgn; i < stpgn + 10; ++i) {
+        if (i <= alpg) {  // i가 총페이지수보다 같거나 작을때 i 출력
+            let iscpg = (i == cpg) ? true : false;  // 현재페이지 표시
+            let pgn = {'num': i, 'iscpg': iscpg};
+            stpgns.push(pgn);
+        }
     }
 
-    let alpg = new Board().selectCount().then((cnt) => cnt);  // 총게시물 수
-
-    let isprev = (cpg - 1 > 0) ? true: false;
-    let isnext = (cpg < alpg) ? true: false;
-    let pgn = {'prev': stpgn - 1, 'next': stpgn + 9 + 1,
-            'isprev': isprev, 'isnext': isnext};
+    let isprev = (cpg - 1 > 0);  // 이전 버튼 표시 여부
+    let isnext = (cpg < alpg);   // 다음 버튼 표시 여부
+    let isprev10 = (cpg - 10 > 0);
+    let isnext10 = (cpg + 10 < alpg);
+    let pgn = {'prev': cpg - 1, 'next': cpg + 1, // 이전 : 현재페이지 - 1, 다음 : 현재페이지 + 1
+            'prev10': cpg - 10, 'next10': cpg + 10,
+            'isprev': isprev, 'isnext': isnext,
+            'isprev10': isprev10, 'isnext10': isnext10};
 
     let bds = new Board().select(stnum).then((bds) => bds);
-    console.log(cpg, stnum, stpgn);
+    console.log(cpg, stnum, stpgn, alpg, isnext);
 
-    res.render('board/list', {title: '게시판 목록',
+    res.render('board/list', { title: '게시판 목록',
             bds: await bds, stpgns: stpgns, pgn: pgn });
 });
 
